a13be8385b277d20f7bb9abae5fab8dd60ffc495
FinnDePuy <103275480+FinnDePuy@users.noreply.github.com>
2024-05-06 13:26:38 -0700

    did a little tweaking now, it feels better

--- MidtermDigitalAudio/Assets/FinishTrigger.cs
+++ MidtermDigitalAudio/Assets/FinishTrigger.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class FinishTrigger : MonoBehaviour
 {
+    [SerializeField] private Canvas endscreen;
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
-                      
+            endscreen.enabled = true;
             //
             //SOUND GOES HERE
             //
